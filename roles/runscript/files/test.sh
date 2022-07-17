e23c3e6971a8919bef9c47190a1b091289b3a21c
iamnvn <nvnckmr@gmail.com>
2022-07-17 06:04:14 -0400

    test

--- roles/runscript/files/test.sh
+++ roles/runscript/files/test.sh
@@ -5,4 +5,5 @@
 #/igs_swdepot/igs/MidrangeDBAServices/DB2/V11.5_Linux/SpecialBuild_requested_upgrades/db2_11.1.4.6_SB/universal/db2_install -b /opt/IBM/db2/V11.1 -p SERVER -l /tmp/install_db2.log -n -y
 
 #/opt/IBM/db2/V11.1/instance/db2iupgrade -u db2udf db2npoc
-startrpdomain db2_hadr_domian
+#startrpdomain db2_hadr_domian
+echo "This is a test script"
